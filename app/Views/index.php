63cba37bc8a9b6cac403368caa3e1d2e21250d07
setsunafjustin00208 <setsunafjustin00208@outlook.com>
2022-05-01 15:33:53 +0900

    Currently on index page

--- app/Views/index.php
+++ app/Views/index.php
@@ -35,12 +35,24 @@
     <link rel="stylesheet" href="<?=base_url('/design/css/bulma.css')?>" type="text/css">
     <link rel="stylesheet" href="<?=base_url('/design/css/animate.min.css')?>" type="text/css">
     <link rel="stylesheet" href="<?=base_url('/design/css/modal-fx.css')?>" type="text/css">
+    <link rel="stylesheet" href="<?=base_url('/design/css/datatables.min.css')?>" type="text/css">
+    <link rel="stylesheet" href="<?=base_url('/design/css/dataTables.bulma.css')?>" type="text/css">
+    <link rel="stylesheet" href="<?=base_url('/design/css/jquery.dataTables.css')?>" type="text/css">
     <script src="<?=base_url('/design/js/mine.js')?>" type="text/javascript"></script>
     <script src="<?=base_url('/design/js/all.js')?>" type="text/javascript"></script>
     <script src="<?=base_url('/design/js/jquery-3.6.0.js')?>" type="text/javascript"></script>
     <script src="<?=base_url('/design/js/popper.min.js')?>" type="text/javascript"></script>
     <script src="<?=base_url('/design/js/modal-fx.js')?>" type="text/javascript"></script>
     <script src="<?=base_url('/design/js/sweetalert2.all.min.js')?>" type="text/javascript"></script>
+    <script src="<?=base_url('/design/js/dataTables.bulma.js')?>" type="text/javascript"></script>
+    <script src="<?=base_url('/design/js/datatables.js')?>" type="text/javascript"></script>
+    <script src="<?=base_url('/design/js/jquery.dataTables.js')?>" type="text/javascript"></script>
+    <script src="<?=base_url('/design/js/acorn_interpreter.js')?>"></script>
+    <script src="<?=base_url('/design/js/blockly_compressed.js')?>"></script>
+    <script src="<?=base_url('/design/js/blocks_compressed.js')?>"></script>
+    <script src="<?=base_url('/design/js/javascript_compressed.js')?>"></script>
+    <script src="<?=base_url('/design/js/msg/js/en.js')?>"></script>
+    <script src="<?=base_url('/design/js/wait_block.js')?>"></script>
 </head>
 <style>
     body, html
@@ -60,6 +72,7 @@
         background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
+        overflow: auto;
     }
     .modal-size
     {
@@ -302,5 +315,214 @@
                         </footer>
                 </div>
         </div>
+        <div class="container mb-6">
+        <div class="container content has-background-light box p-3 mt-5">
+            <h1>Hello World</h1>
+            <p>Lorem ipsum<sup><a>[1]</a></sup> dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque. Sub<sub>script</sub> works as well!</p>
+            <h2>Second level</h2>
+            <p>Curabitur accumsan turpis pharetra <strong>augue tincidunt</strong> blandit. Quisque condimentum maximus mi, sit amet commodo arcu rutrum id. Proin pretium urna vel cursus venenatis. Suspendisse potenti. Etiam mattis sem rhoncus lacus dapibus facilisis. Donec at dignissim dui. Ut et neque nisl.</p>
+            <ul>
+                <li>In fermentum leo eu lectus mollis, quis dictum mi aliquet.</li>
+                <li>Morbi eu nulla lobortis, lobortis est in, fringilla felis.</li>
+                <li>Aliquam nec felis in sapien venenatis viverra fermentum nec lectus.</li>
+                <li>Ut non enim metus.</li>
+            </ul>     
+        </div>
+        <div class="container has-background-light p-3 box">
+                <div class="buttons">
+                    <button class="button is-link p-3 m-3" onclick="runCode()" id="runButton"><i class="fa-solid fa-terminal"></i> &nbsp; Run Program</button>
+                </div>
+                <div class="columns" style="width: 100%">
+                    <div class="column" id="blocklyDiv"
+                        style="display: inline-block; height: 480px; width: 68%"></div>
+                    <div class="column ">
+                    <textarea name="a_output" id="output"
+                        style="display: inline-block; height: 455px;" readonly class="textarea has-fixed-size has-text-black is-link">
+                    </textarea>
+                    </div>
+                </div>
+                <xml xmlns="https://developers.google.com/blockly/xml" id="toolbox" style="display: none">
+                    <category name="Logic" colour="%{BKY_LOGIC_HUE}">
+                    <block type="controls_if"></block>
+                    <block type="logic_compare"></block>
+                    <block type="logic_operation"></block>
+                    <block type="logic_negate"></block>
+                    <block type="logic_boolean"></block>
+                    </category>
+                    <category name="Loops" colour="%{BKY_LOOPS_HUE}">
+                    <block type="controls_repeat_ext">
+                        <value name="TIMES">
+                        <block type="math_number">
+                            <field name="NUM">10</field>
+                        </block>
+                        </value>
+                    </block>
+                    <block type="controls_whileUntil"></block>
+                    </category>
+                    <category name="Math" colour="%{BKY_MATH_HUE}">
+                    <block type="math_number">
+                        <field name="NUM">123</field>
+                    </block>
+                    <block type="math_arithmetic"></block>
+                    <block type="math_single"></block>
+                    </category>
+                    <category name="Text" colour="%{BKY_TEXTS_HUE}">
+                    <block type="text"></block>
+                    <block type="text_length"></block>
+                    <block type="text_print"></block>
+                    <block type="text_prompt_ext">
+                        <value name="TEXT">
+                        <block type="text"></block>
+                        </value>
+                    </block>
+                    </category>
+                    <sep></sep>
+                    <category name="Variables" custom="VARIABLE" colour="%{BKY_VARIABLES_HUE}">
+                    </category>
+                    
+                </xml>
+
+                <xml xmlns="https://developers.google.com/blockly/xml" id="startBlocks" style="display: none">
+                    
+                </xml>
+
+                <script>
+                    var demoWorkspace = Blockly.inject('blocklyDiv',
+                        {media: '<?=base_url()?>/design/media/',
+                        toolbox: document.getElementById('toolbox')});
+                    Blockly.Xml.domToWorkspace(document.getElementById('startBlocks'),
+                                            demoWorkspace);
+
+                    // Exit is used to signal the end of a script.
+                    Blockly.JavaScript.addReservedWords('exit');
+
+                    var outputArea = document.getElementById('output');
+                    var runButton = document.getElementById('runButton');
+                    var myInterpreter = null;
+                    var runner;
+
+                    function initApi(interpreter, globalObject) {
+                    // Add an API function for the alert() block, generated for "text_print" blocks.
+                    var wrapper = function(text) {
+                        text = text ? text.toString() : '';
+                        outputArea.value = outputArea.value + '\n' + text;
+                    };
+                    interpreter.setProperty(globalObject, 'alert',
+                        interpreter.createNativeFunction(wrapper));
+
+                    // Add an API function for the prompt() block.
+                    var wrapper = function(text) {
+                        text = text ? text.toString() : '';
+                        return interpreter.createPrimitive(prompt(text));
+                    };
+                    interpreter.setProperty(globalObject, 'prompt',
+                        interpreter.createNativeFunction(wrapper));
+
+                    // Add an API for the wait block.  See wait_block.js
+                    initInterpreterWaitForSeconds(interpreter, globalObject);
+
+                    // Add an API function for highlighting blocks.
+                    var wrapper = function(id) {
+                        id = id ? id.toString() : '';
+                        return interpreter.createPrimitive(highlightBlock(id));
+                    };
+                    interpreter.setProperty(globalObject, 'highlightBlock',
+                        interpreter.createNativeFunction(wrapper));
+                    }
+
+                    var highlightPause = false;
+                    var latestCode = '';
+
+                    function highlightBlock(id) {
+                    demoWorkspace.highlightBlock(id);
+                    highlightPause = true;
+                    }
+
+                    function resetStepUi(clearOutput) {
+                    demoWorkspace.highlightBlock(null);
+                    highlightPause = false;
+                    runButton.disabled = '';
+
+                    if (clearOutput) {
+                        outputArea.value = 'Program output:\n=================\n';
+                    }
+                    }
+
+                    function generateCodeAndLoadIntoInterpreter() {
+                    // Generate JavaScript code and parse it.
+                    Blockly.JavaScript.STATEMENT_PREFIX = 'highlightBlock(%1);\n';
+                    Blockly.JavaScript.addReservedWords('highlightBlock');
+                    latestCode = Blockly.JavaScript.workspaceToCode(demoWorkspace);
+
+                    resetStepUi(true);
+                    }
+
+                    function resetInterpreter() {
+                    myInterpreter = null;
+                    if (runner) {
+                        clearTimeout(runner);
+                        runner = null;
+                    }
+                    }
+
+                    function runCode() {
+                    if (!myInterpreter) {
+                        // First statement of this code.
+                        // Clear the program output.
+                        resetStepUi(true);
+                        runButton.disabled = 'disabled';
+
+                        // And then show generated code in an alert.
+                        // In a timeout to allow the outputArea.value to reset first.
+                        setTimeout(function() {
+                            alert('Ready to execute the following code\n' +
+                        '===================================\n' +
+                        latestCode);
+
+                        // Begin execution
+                        highlightPause = false;
+                        myInterpreter = new Interpreter(latestCode, initApi);
+                        runner = function() {
+                            if (myInterpreter) {
+                            var hasMore = myInterpreter.run();
+                            if (hasMore) {
+                                // Execution is currently blocked by some async call.
+                                // Try again later.
+                                setTimeout(runner, 10);
+                            } else {
+                                // Program is complete.
+                                outputArea.value += '\n\n<< Program complete >>';
+                                resetInterpreter();
+                                resetStepUi(false);
+                            }
+                            }
+                        };
+                        runner();
+                        }, 1);
+                        return;
+                    }
+                    }
+
+                    // Load the interpreter now, and upon future changes.
+                    generateCodeAndLoadIntoInterpreter();
+                    demoWorkspace.addChangeListener(function(event) {
+                    if (!(event instanceof Blockly.Events.Ui)) {
+                        // Something changed. Parser needs to be reloaded.
+                        resetInterpreter();
+                        generateCodeAndLoadIntoInterpreter();
+                    }
+                    });
+                </script>
+        </div>
+        </div>
+        <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+            <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+            is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+            </p>
+        </div>
+        </footer>
 </body>
 </html>
